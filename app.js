711e1209b7bccaa7f36d14c17a7ac2129ca32569
LJBL22 <ljbl2208@gmail.com>
2023-07-10 10:25:40 +0800

    API: IntersecOb I

--- app.js
+++ app.js
@@ -5,18 +5,31 @@ const INDEX_URL = `${BASE_URL}?limit=${limit}&select=${select}`
 const productPanel = document.querySelector('#product-wrapper')
 const productList = []
 
-// 使用原生 fetch 方法
 function getProducts() {
   fetch(INDEX_URL)
-    // 返回 Promise，resolves 輸出成 JSON 物件
     .then(res => res.json())
-    // // .then(data => console.log(data)) 相等於下面這行，直接印出 Promise 傳來的結果
-    // .then(console.log)
-    // 前一個 Promise 成功 return 的值是下一個 chain 調用的參數
     .then(data => {
       const items = data.products
       productList.push(...items);
       renderProducts(productList)
+
+      // 選到每一個 item-card
+      const targetList = document.querySelectorAll('.item-card')
+      const observer = new IntersectionObserver(callbackFunc, {
+        threshold: .6
+      })
+
+      function callbackFunc(entries, observer) {
+        // 現在的問題是沒有一個在可視範圍(值為false)，因為全部在最剛開始都是沒有 show 的。
+        // => display: none 會使 InterOb 觀察不到該元素，進而無法觸發後續 callback func
+        // iterate 每一個 entry ，如果在可視範圍 isInter: true，即加上 show opacity: 1
+        entries.forEach(entry => {
+          entry.target.classList.toggle("show", entry.isIntersecting);
+          // 讓已出現的元素保留
+          if (entry.isIntersecting) observer.unobserve(entry.target)
+        })
+      }
+      targetList.forEach(target => { observer.observe(target) })
     })
     .catch((error) => console.log("Error:", error.message))
 }
